16e3b3729afaa657a2210c2d5928f9367f0283d7
Ching Man Leung <ching.leung@bynd.com>
2018-08-20 10:09:44 +0100

    initial testing

--- app/Application.jsx
+++ app/Application.jsx
@@ -1,4 +1,5 @@
 import React, {createClass, PropTypes} from 'react';
+import ReactDOM from 'react-dom';
 
 const Header = ({title}) => <h1>{title}</h1>;
 
@@ -10,3 +11,68 @@ export default createClass({
     return <Header {...this.props.state}/>;
   }
 });
+
+class Square extends React.Component {
+    render() {
+      return (
+        <button className="square">
+          {this.props.value}
+        </button>
+      );
+    }
+}
+  
+class Board extends React.Component {
+    renderSquare(i) {
+        return <Square value={i} />;
+    }
+
+    render() {
+        const status = 'Next player: X';
+
+        return (
+        <div>
+            <div className="status">{status}</div>
+            <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+            </div>
+            <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+            </div>
+            <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+            </div>
+        </div>
+        );
+    }
+}
+  
+class Game extends React.Component {
+    render() {
+        return (
+        <div className="game">
+            <div className="game-board">
+            <Board />
+            </div>
+            <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+            </div>
+        </div>
+        );
+    }
+}
+
+// ========================================
+if(typeof window !== 'undefined') {
+    ReactDOM.render(
+        <Game />,
+        document.getElementById('root')
+    );
+}
